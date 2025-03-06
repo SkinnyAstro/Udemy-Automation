6f781727c359636c1c21032963333fea7ec447df
Skinny Astro <sartapeprasad@gmail.com>
2025-03-06 15:24:41 +0530

    changes added

--- src/main/java/com/selenium/practice/App.java
+++ src/main/java/com/selenium/practice/App.java
@@ -15,16 +15,22 @@ public class App
         WebDriver driver = new ChromeDriver();
         driver.get("https://practicetestautomation.com/practice-test-login/");
         WebElement UsernameField = driver.findElement(By.id("username"));
+        WebElement username = driver.findElement(By.xpath("//input[@id='username']")); // using the xpath
+        WebElement name = driver.findElement(By.cssSelector("input[id='username']")); // using CSS Selector.
         WebElement PasswordField = driver.findElement(By.id("password"));
+        WebElement passwordcss = driver.findElement(By.cssSelector("input[id='password']"));
         WebElement SubmitBtn = driver.findElement(By.className("btn"));
+        WebElement button = driver.findElement(By.xpath("//button[@class='btn']"));
 
-        List<WebElement> inputFields = driver.findElements(By.tagName("input"));
+        List<WebElement> inputFields = driver.findElements(By.tagName("input")); // it wll list all the input type webelements
+
 
         WebElement LinkTextLocator = driver.findElement(By.linkText("Practice Test Automation."));
         WebElement PartialLinkText = driver.findElement(By.partialLinkText("Practice Test"));
 
         WebElement passwordbelowusername = driver.findElement(RelativeLocator.with(By.tagName("input")).below(By.id("username")));
         WebElement PrivacyPolicyLink = driver.findElement(RelativeLocator.with(By.tagName("a")).straightRightOf(By.linkText("Practice Test Automation.")));
+        WebElement Homebutton = driver.findElement(By.xpath("menu-item-43"));
 
         driver.quit();
     }
