0338464e1f1439287b0794a3bad116228da53b68
Skinny Astro <sartapeprasad@gmail.com>
2025-03-07 14:24:56 +0530

    New code added

--- src/main/java/com/selenium/practice/App.java
+++ src/main/java/com/selenium/practice/App.java
@@ -13,6 +13,13 @@ public class App
     public static void main( String[] args )
     {
         WebDriver driver = new ChromeDriver();
+        loginpageElemet(driver);
+        Exceptionpageelement(driver);
+        driver.quit();
+
+    }
+
+    private static String  loginpageElemet(WebDriver driver){
         driver.get("https://practicetestautomation.com/practice-test-login/");
         WebElement UsernameField = driver.findElement(By.id("username"));
         WebElement username = driver.findElement(By.xpath("//input[@id='username']")); // using the xpath
@@ -30,9 +37,30 @@ public class App
 
         WebElement passwordbelowusername = driver.findElement(RelativeLocator.with(By.tagName("input")).below(By.id("username")));
         WebElement PrivacyPolicyLink = driver.findElement(RelativeLocator.with(By.tagName("a")).straightRightOf(By.linkText("Practice Test Automation.")));
-        WebElement Homebutton = driver.findElement(By.xpath("menu-item-43"));
+        //WebElement Homebutton = driver.findElement(By.xpath("menu-item-43"));
+        return(null);
 
-        driver.quit();
+    }
+
+    private static void Exceptionpageelement(WebDriver driver){
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/");
+        // find webelement for link Selenium webdriver with java for beginners program uisng link text and partial link text
+        WebElement fulllink = driver.findElement(By.linkText("Selenium WebDriver with Java for beginners program"));
+        WebElement partiallink = driver.findElement(By.partialLinkText("Selenium WebDriver with Java"));
+        // find webelement for input fields using tag class name, xpath and CSS.
+
+        // create a list of webelement for all buttons using tag.
+        List<WebElement>buttonfields = driver.findElements(By.tagName("button"));
+        // Identify both buttons using ID, name , CSS and Xpath for each
+        WebElement editbuttonid = driver.findElement(By.id("edit_btn"));
+        WebElement editbuttonname = driver.findElement(By.name("Edit"));
+        WebElement editbuttonXpath = driver.findElement(By.xpath("//button[@id='edit_btn']"));
+        WebElement editbuttonCSS = driver.findElement(By.cssSelector("button[id='edit_btn']"));
+
+        WebElement Addbuttonid = driver.findElement(By.id("add_btn"));
+        WebElement Addbyuttonname = driver.findElement(By.name("Add"));
+        WebElement Addbuttonxpath = driver.findElement(By.xpath("//button[@id='add_btn']"));
+        WebElement AddbuttonCSS = driver.findElement(By.cssSelector("button[id='add_btn']"));
     }
     private static String ChromeTest(String url){
         WebDriver driver = new ChromeDriver();
