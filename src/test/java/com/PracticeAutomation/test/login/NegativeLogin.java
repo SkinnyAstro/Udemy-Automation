98b108728de598cab1048e12d7b583f063a6d7f5
Skinny Astro <sartapeprasad@gmail.com>
2025-03-10 15:56:27 +0530

    NEW CODE ADDED

--- src/test/java/com/PracticeAutomation/test/login/NegativeLogin.java
+++ src/test/java/com/PracticeAutomation/test/login/NegativeLogin.java
@@ -1,5 +1,4 @@
 package com.PracticeAutomation.test.login;
-
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,7 +8,7 @@ import org.testng.Assert;
 
 public class NegativeLogin {
     @Test
-    public void IncorrectUsername(){
+    public void IncorrectUsername() throws InterruptedException{
         WebDriver driver = new ChromeDriver();
 // Open page
         driver.get("https://practicetestautomation.com/practice-test-login/");
@@ -23,18 +22,27 @@ public class NegativeLogin {
 //Push Submit button
         WebElement submitbtn = driver.findElement(By.id("submit"));
         submitbtn.click();
-
+        Thread.sleep(2000);
 //Verify error message is displayed
-        String pageSource = driver.getPageSource();
-        String Message = "Your username is invalid!";
-        Assert.assertTrue(pageSource.contains(Message)); // using an Assert boolean method to match the Error message from the page source.
-//Verify error message text is Your username is invalid!
+
+        WebElement Error = driver.findElement(By.id("error"));
+        Assert.assertTrue(Error.isDisplayed()); // We are using the assert condition to see if the error element is displayed or not
+        //Verify error message text is Your username is invalid!
+//        String pageSource = driver.getPageSource();
+//        String Message = "Your username is invalid!";
+//        Assert.assertTrue(pageSource.contains(Message)); // using an Assert boolean method to match the Error message from the page source.
+
+        String ExpectedErrorMessage ="Your username is invalid!";
+        String ActualErrormessage = Error.getText(); // we are creating an varibale of Actual message and storing the value that is being passed in the Error element
+        Assert.assertEquals(ActualErrormessage ,ExpectedErrorMessage);
+        driver.quit();
+
         driver.quit();
 
     }
 
     @Test
-    public void IncorrectPassword(){
+    public void IncorrectPassword() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://practicetestautomation.com/practice-test-login/");
 
@@ -46,11 +54,18 @@ public class NegativeLogin {
 
         WebElement submitbtn = driver.findElement(By.id("submit"));
         submitbtn.click();
+        Thread.sleep(2000);
 
-        String ErrorMessage = "Your password is invalid!";
-        String pageSource = driver.getPageSource();
-        Assert.assertTrue(pageSource.contains(ErrorMessage));
+        WebElement Error = driver.findElement(By.id("error"));
+        Assert.assertTrue(Error.isDisplayed());
 
+        String ExpectedErrorMessage = "Your password is invalid!";
+        String ActualErrorMessage = Error.getText();
+
+//        String pageSource = driver.getPageSource();
+//        Assert.assertTrue(pageSource.contains(ErrorMessage));
+
+        Assert.assertEquals(ActualErrorMessage, ExpectedErrorMessage);
         driver.quit();
     }
 }
